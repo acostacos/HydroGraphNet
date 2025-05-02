6f1fadfff67f8d5108d2875fd6b7eae307d2d223
acostacos <acostacarlom@gmail.com>
2025-05-02 09:25:05 +0800

    Update default mode for wandb

--- examples/weather/flood_modeling/hydrographnet/train.py
+++ examples/weather/flood_modeling/hydrographnet/train.py
@@ -231,13 +231,13 @@ class MGNTrainer:
 def main(cfg: DictConfig) -> None:
     DistributedManager.initialize()
     dist = DistributedManager()
-    # initialize_wandb(
-    #     project="Modulus-Launch",
-    #     entity="Modulus",
-    #     name="Vortex_Shedding-Training",
-    #     group="Vortex_Shedding-DDP-Group",
-    #     mode=cfg.wandb_mode,
-    # )
+    initialize_wandb(
+        project="Modulus-Launch",
+        entity="Modulus",
+        name="Vortex_Shedding-Training",
+        group="Vortex_Shedding-DDP-Group",
+        mode=cfg.wandb_mode,
+    )
     logger = PythonLogger("main")
     rank_zero_logger = RankZeroLoggingWrapper(logger, dist)
     rank_zero_logger.file_logging()
