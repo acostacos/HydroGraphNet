fe39aa06d169cec201664bd367da0a5796a38835
acostacos <acostacarlom@gmail.com>
2025-05-10 09:17:52 +0800

    Update dataset transformation from hecras; Still need to implement volume and inflow

--- create_dataset_from_hecras.py
+++ create_dataset_from_hecras.py
@@ -1,164 +1,125 @@
+import os
 import numpy as np
-import torch
 
-from transform_helper_files.hecras_data_retrieval import get_cell_area, get_water_level, get_facepoint_coordinates, get_velocity,\
-    get_edge_direction_x, get_edge_direction_y, get_face_length, get_facecell_indexes, get_facepoint_indexes
-from transform_helper_files.shp_data_retrieval import get_cell_position, get_cell_elevation, get_edge_index, get_edge_length, get_edge_slope
+from transform_helper_files.hecras_data_retrieval import get_cell_area, get_water_level, get_roughness, get_rainfall
+from transform_helper_files.shp_data_retrieval import get_cell_position, get_cell_elevation
 
-def get_cell_velocity(hec_ras_filepath: str, node_shp_filepath: str, perimeter_name: str = 'Perimeter 1') -> torch.Tensor:
-    '''Adopted from https://doi.org/10.26188/24312658'''
-    def dist_center2faces(center_xy,faces_xy):
-        dist = np.sqrt(np.square(faces_xy[:,0]-center_xy[0]) + np.square(faces_xy[:,1]-center_xy[1]))
-        return dist
+def get_cell_slope():
+    cell_slope_np_path = ''
+    return np.load(cell_slope_np_path)
 
-    xy_coor = get_cell_position(node_shp_filepath)
-    cell_area = get_cell_area(hec_ras_filepath, perimeter_name)
-    facepoint_xy_coor = get_facepoint_coordinates(hec_ras_filepath, perimeter_name)
-    edge_direction_x = get_edge_direction_x(hec_ras_filepath, perimeter_name)
-    edge_direction_y = get_edge_direction_y(hec_ras_filepath, perimeter_name)
-    face_length = get_face_length(hec_ras_filepath, perimeter_name)
-    faces_cell_idx = get_facecell_indexes(hec_ras_filepath, perimeter_name)
-    faces_facepoint_idx = get_facepoint_indexes(hec_ras_filepath, perimeter_name)
-    face_vel = get_velocity(hec_ras_filepath, perimeter_name)
+def get_cell_aspect():
+    cell_aspect_np_path = ''
+    return np.load(cell_aspect_np_path)
 
-    # Find x-y components for each cell face in each cell
-    n_timesteps = len(face_vel)
-    n_cells = len(xy_coor)
+def get_cell_curvature():
+    cell_curvature_np_path = ''
+    return np.load(cell_curvature_np_path)
 
-    cell_velocity_x = np.zeros([n_timesteps, n_cells])
-    cell_velocity_y = np.zeros([n_timesteps, n_cells])
-    for cell_i in range(n_cells):
-        # Find cell in FROM/TO table of faces
-        find_faces_for_cell = np.column_stack(np.where(faces_cell_idx == cell_i))
+def get_cell_infiltration(num_nodes: int):
+    return np.zeros(num_nodes)
 
-        # Find cell velocity x-y components each cell
-        # HEC-RAS method: https://www.hec.usace.army.mil/confluence/rasdocs/ras1dtechref/latest/theoretical-basis-for-one-dimensional-and-two-dimensional-hydrodynamic-calculations/2d-unsteady-flow-hydrodynamics/numerical-methods/cell-velocity
-        # Vc = 1/A * SUM(dx * L * v_f)
-        # where A is the cell area, 
-        # dx is distance from cell center to facecenter, 
-        # L is face length, v_f is facevelocity
-        cell_xy = xy_coor[cell_i]
-        facepoint1_xy = facepoint_xy_coor[faces_facepoint_idx[find_faces_for_cell[:,0]][:,0]]
-        facepoint2_xy = facepoint_xy_coor[faces_facepoint_idx[find_faces_for_cell[:,0]][:,1]]
-        face_center_xy = np.c_[np.mean([facepoint1_xy[:,0], facepoint2_xy[:,0]], axis=0),
-                    np.mean([facepoint1_xy[:,1], facepoint2_xy[:,1]], axis=0)]
+def get_cell_flow_accumulation():
+    cell_flow_accumulation_np_path = ''
+    return np.load(cell_flow_accumulation_np_path)
 
-        dx_center2face = dist_center2faces(cell_xy,face_center_xy)
+def create_hydrograph_id_files(event_keys: list[str], dataset_folder: str):
+    for event_key in event_keys:
+        train_event_keys = [k for k in event_keys if k != event_key]
+        train_filename = f'train_for_{event_key}.txt'
+        train_path = os.path.join(dataset_folder, train_filename)
+        with open(train_path, 'w') as f:
+            for train_key in train_event_keys:
+                f.write(f"{train_key}")
 
-        if not cell_area[cell_i].any():
-            continue
-        else:
-            cell_velocity_x[:,cell_i] = 1/cell_area[cell_i] * np.sum(dx_center2face * 
-                                                face_length[find_faces_for_cell[:,0]] * 
-                                                face_vel[:, find_faces_for_cell[:,0]] * edge_direction_x[find_faces_for_cell[:,0]], axis=1)
+        test_filename = f'test_for_{event_key}.txt'
+        test_path = os.path.join(dataset_folder, test_filename)
+        with open(test_path, 'w') as f:
+            f.write(f"{event_key}")
 
-            cell_velocity_y[:,cell_i] = 1/cell_area[cell_i] * np.sum(dx_center2face *
-                                                    face_length[find_faces_for_cell[:,0]] *
-                                                    face_vel[:, find_faces_for_cell[:,0]] * edge_direction_y[find_faces_for_cell[:,0]], axis=1)
+def create_constant_text_files(hec_ras_filepath: str, node_shp_filepath: str, dem_path: str, dataset_folder: str, prefix: str):
+    pos = get_cell_position(node_shp_filepath)
+    pos_path = os.path.join(dataset_folder, f"{prefix}_XY.txt")
+    np.savetxt(pos_path, pos)
 
-    return torch.FloatTensor(cell_velocity_x), torch.FloatTensor(cell_velocity_y)
+    area = get_cell_area(hec_ras_filepath)
+    area_path = os.path.join(dataset_folder, f"{prefix}_CA.txt")
+    np.savetxt(area_path, area)
 
-def get_cell_slope(hec_ras_filepath: str, edge_shp_path: str, edge_index: torch.Tensor, n_cells: int, perimeter_name: str = 'Perimeter 1') -> tuple[torch.Tensor, torch.Tensor]:
-    edge_slope = get_edge_slope(edge_shp_path)
-    edge_direction_x = get_edge_direction_x(hec_ras_filepath, perimeter_name)
-    edge_direction_y = get_edge_direction_y(hec_ras_filepath, perimeter_name)
+    elevation = get_cell_elevation(node_shp_filepath)
+    elevation_path = os.path.join(dataset_folder, f"{prefix}_CE.txt")
+    np.savetxt(elevation_path, elevation)
 
-    # Compute slope in x and y directions
-    edge_slope_x = edge_slope * edge_direction_x
-    edge_slope_y = edge_slope * edge_direction_y
+    manning = get_roughness(hec_ras_filepath)
+    manning_path = os.path.join(dataset_folder, f"{prefix}_N.txt")
+    np.savetxt(manning_path, manning)
 
-    slope_x = np.zeros((n_cells))
-    slope_y = np.zeros((n_cells))
-    for cell_i in range(n_cells):
-        cell_edge_idx = ((edge_index[0] == cell_i) | (edge_index[1] == cell_i)).nonzero()
+    slope = get_cell_slope()
+    slope_path = os.path.join(dataset_folder, f"{prefix}_CS.txt")
+    np.savetxt(slope_path, slope)
 
-        # Slope for each cell is the average of the slopes of the edges connected to it
-        slope_x[cell_i] = np.average(edge_slope_x[cell_edge_idx])
-        slope_y[cell_i] = np.average(edge_slope_y[cell_edge_idx])
+    aspect = get_cell_aspect()
+    aspect_path = os.path.join(dataset_folder, f"{prefix}_A.txt")
+    np.savetxt(aspect_path, aspect)
 
-    return torch.FloatTensor(slope_x), torch.FloatTensor(slope_y)
+    curvature = get_cell_curvature()
+    curvature_path = os.path.join(dataset_folder, f"{prefix}_CU.txt")
+    np.savetxt(curvature_path, curvature)
 
-def get_edge_relative_distance(pos: torch.Tensor, edge_index: torch.Tensor) -> torch.Tensor:
-    row, col = edge_index
-    edge_relative_distance = pos[col] - pos[row]
-    return edge_relative_distance
+    infiltration = get_cell_infiltration(len(pos))
+    infiltration_path = os.path.join(dataset_folder, f"{prefix}_IP.txt")
+    np.savetxt(infiltration_path, infiltration)
 
-def get_dataset_features(hec_ras_file_path: str, node_shp_path: str, edge_shp_path: str) -> dict:
-    # HEC-RAS data retrieval
-    water_level = torch.FloatTensor(get_water_level(hec_ras_file_path))
-    cell_velocity_x, cell_velocity_y = get_cell_velocity(hec_ras_file_path, node_shp_path)
+    flow_accum = get_cell_flow_accumulation()
+    flow_accum_path = os.path.join(dataset_folder, f"{prefix}_FA.txt")
+    np.savetxt(flow_accum_path, flow_accum)
 
-    # Node Shapefile data retrieval
-    dem = torch.FloatTensor(get_cell_elevation(node_shp_path))
-    water_depth = water_level - dem
-    pos = torch.FloatTensor(get_cell_position(node_shp_path))
+def create_dynamic_text_files(hec_ras_filepath: str, node_shp_filepath: str, dataset_folder: str, prefix: str, hydrograph_id: str):
+    water_level = get_water_level(hec_ras_filepath)
+    elevation = get_cell_elevation(node_shp_filepath)
+    water_depth = np.clip(water_level - elevation, a_min=0)
+    assert np.all(water_depth > 0)
+    water_depth_path = os.path.join(dataset_folder, f"{prefix}_WD_{hydrograph_id}.txt")
+    np.savetxt(water_depth_path, water_depth)
 
-    # Edge Shapefile data retrieval
-    edge_index = torch.LongTensor(get_edge_index(edge_shp_path))
-    edge_distance = torch.FloatTensor(get_edge_length(edge_shp_path))
-    slope_x, slope_y = get_cell_slope(hec_ras_file_path, edge_shp_path, edge_index, len(dem))
+    precipitation = get_rainfall(hec_ras_filepath)
+    precipitation_path = os.path.join(dataset_folder, f"{prefix}_Pr_{hydrograph_id}.txt")
+    np.savetxt(precipitation_path, precipitation)
 
-    # Convert edges to undirected
-    edge_distance = torch.cat([edge_distance, edge_distance], dim=0)
-    edge_relative_distance = get_edge_relative_distance(pos, edge_index)
-
-    # Get graph features
-    num_nodes = len(dem)
-
-    return {
-        'num_nodes': num_nodes,
-        'edge_index': edge_index,
-        'water_depth': water_depth,
-        'cell_velocity_x': cell_velocity_x,
-        'cell_velocity_y': cell_velocity_y,
-        'dem': dem,
-        'pos': pos,
-        'slope_x': slope_x,
-        'slope_y': slope_y,
-        'edge_distance': edge_distance,
-        # 'edge_slope': edge_slope,
-        'edge_relative_distance': edge_relative_distance,
-    }
+    # TODO: How to get inflow and volume?
+    # inflow_path = os.path.join(folder, f"{prefix}_US_InF_{hydrograph_id}.txt")
+    # volume_path = os.path.join(folder, f"{prefix}_V_{hydrograph_id}.txt")
 
 def main():
-    datasets = {
-        'event_key': {
-            'hec_ras_file_path': "path/to/hec_ras/file",
-            'node_shp_path': "path/to/node/shp/file",
-            'edge_shp_path': "path/to/edge/shp/file",
-        },
+    prefix = "init"
+    base_dataset_floder = f"outputs_phy/hecras_data/{prefix}"
+    node_shp_path = "C:\\Users\\Carlo\\Documents\\School\\Masters\\NUS\\gnn_flood_modeling\\data\\datasets\\init\\initp01\\raw\\cell_centers.shp"
+    dem_path = "C:\\Users\\Carlo\\Documents\\School\\Masters\\NUS\\Dissertation\\Data\\Wollombi_Model_LR\\DEM_5m_EPSG28356_ND999_Wollombi.tif"
+    hec_ras_file_paths = {
+        'p01': "C:\\Users\\Carlo\\Documents\\School\\Masters\\NUS\\gnn_flood_modeling\\data\\datasets\\init\\initp01\\raw\\Model_01.p01.hdf",
+        'p02': "C:\\Users\\Carlo\\Documents\\School\\Masters\\NUS\\gnn_flood_modeling\\data\\datasets\\init\\initp02\\raw\\Model_01.p02.hdf",
+        'p03': "C:\\Users\\Carlo\\Documents\\School\\Masters\\NUS\\gnn_flood_modeling\\data\\datasets\\init\\initp03\\raw\\Model_01.p03.hdf",
+        'p04': "C:\\Users\\Carlo\\Documents\\School\\Masters\\NUS\\gnn_flood_modeling\\data\\datasets\\init\\initp04\\raw\\Model_01.p04.hdf",
     }
 
-    base_dataset_floder = "hecras_datasets"
+    # Create dataset folder
+    if not os.path.exists(base_dataset_floder):
+        os.makedirs(base_dataset_floder)
+    print(f"Saving files in folder: {base_dataset_floder}", flush=True)
 
-    dataset_keys = list(datasets.keys())
-    for key in dataset_keys:
-        # Create dataset folder
-        dataset_folder = f"{base_dataset_floder}/test_on_{key}"
-        create_dataset_folders(dataset_folder=dataset_folder)
-        print(f"Dataset folder created in: {dataset_folder}", flush=True)
+    print(f"Creating hydrograph id files...", flush=True)
+    event_keys = list(hec_ras_file_paths.keys())
+    create_hydrograph_id_files(event_keys, base_dataset_floder)
 
-    for key in dataset_keys:
-        print(f"Processing event {key}", flush=True)
+    # Create static text files; Can use arbitrary hec_ras file as they are the same
+    print(f"Saving constant features...", flush=True)
+    sample_hec_ras_path = next(iter(hec_ras_file_paths.values()))
+    create_constant_text_files(sample_hec_ras_path, node_shp_path, dem_path, base_dataset_floder, prefix)
 
-        paths = datasets[key]
-        hec_ras_file_path = paths['hec_ras_file_path']
-        node_shp_path = paths['node_shp_path']
-        edge_shp_path = paths['edge_shp_path']
-
-        dataset_features = get_dataset_features(hec_ras_file_path, node_shp_path, edge_shp_path)
-        print(f"\tFinished obtaining features for event {key}", flush=True)
-        pyg_dataset = [convert_to_pyg(dataset_features)]
-
-        train_keys = [k for k in dataset_keys if k != key]
-        for train_key in train_keys:
-            print(f"\tSaving event {key} as train dataset for {train_key}", flush=True)
-            train_folder = f"{base_dataset_floder}/test_on_{train_key}/train"
-            save_database(pyg_dataset, name=key, out_path=train_folder)
-
-        print(f"\tSaving event {key} as test dataset for {key}", flush=True)
-        test_folder = f"{base_dataset_floder}/test_on_{key}/test"
-        save_database(pyg_dataset, name=key, out_path=test_folder)
+    # Create dynamic text files
+    for event_key, hec_ras_file_path in hec_ras_file_paths.items():
+        print(f"Saving dynamic features for event {event_key}...", flush=True)
+        create_dynamic_text_files(hec_ras_file_path, node_shp_path, base_dataset_floder, prefix, hydrograph_id=event_key)
 
 if __name__ == "__main__":
     main()
